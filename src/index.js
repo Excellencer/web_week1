bcaa9b29366f74bd61a70334526437ce23fb7db6
Kalle Liljestrom <54841086+Excellencer@users.noreply.github.com>
2022-09-09 10:11:01 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+
+/* From course video JavaScript 1 */
+if (document.readyState !== "loading") {
+  initializeCode();
+} else {
+  document.addEventlistener("DOMContentLoaded", function () {
+    initializeCode();
+  });
+}
+
+function initializeCode() {
+  document.getElementById("my-button").addEventListener("click", myNotebook);
+  document.getElementById("add-data").addEventListener("click", customText);
+}
+
+function myNotebook() {
+  console.log("hello world");
+  document.getElementById("change").innerHTML = "My Notebook";
+}
+
+function customText() {
+  let areaText = document.getElementById("custom-text").value;
+  document.getElementById("custom-text").value = "";
+  let ul = document.getElementById("un-list");
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(areaText));
+  ul.appendChild(li);
+}
